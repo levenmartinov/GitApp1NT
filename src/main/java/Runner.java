2112e5880b84090047b4a163af9b54f038451b28
levenmartinov <levkomartinov@gmail.com>
2024-09-23 17:13:58 +0100

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,13 @@
+public class Runner {
+    public static void main(String[] args) {
+
+
+        System.out.println("version 1");
+        System.out.println("version 2");
+        System.out.println("version 2 nin devami");
+        System.out.println("version 3");
+        System.out.println("version 4");
+
+
+    }
+}
